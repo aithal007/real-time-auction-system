a9a608fe26f0564916fb882c11e78bdcaba5d912
aithal007 <parikshith.aithal@iiitb.ac.in>
2026-06-27 12:44:42 +0530

    create tcp connection between client and server

--- client.c
+++ client.c
@@ -1,6 +1,78 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "common.h"
+
+#define SERVER_PORT 8080
+
+static void show_menu(void) {
+    puts("\n--- Auction System ---");
+    puts("1. Login");
+    puts("2. Register");
+    puts("3. Create Auction");
+    puts("4. View Auctions");
+    puts("5. Place Bid");
+    puts("6. My Auctions");
+    puts("7. View Winners");
+    puts("8. Exit");
+    printf("Enter choice: ");
+}
 
 int main(void) {
-    puts("Auction client starter");
+    int sock;
+    struct sockaddr_in server_addr;
+    char buffer[256];
+    Request req;
+    int choice;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+        perror("inet_pton");
+        close(sock);
+        return 1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("connect");
+        close(sock);
+        return 1;
+    }
+
+    memset(buffer, 0, sizeof(buffer));
+    if (recv(sock, buffer, sizeof(buffer) - 1, 0) > 0) {
+        printf("%s\n", buffer);
+    }
+
+    show_menu();
+    if (scanf("%d", &choice) != 1) {
+        close(sock);
+        return 1;
+    }
+
+    memset(&req, 0, sizeof(req));
+    req.type = choice;
+    snprintf(req.data, sizeof(req.data), "choice=%d", choice);
+
+    if (send(sock, &req, sizeof(req), 0) < 0) {
+        perror("send");
+        close(sock);
+        return 1;
+    }
+
+    close(sock);
     return 0;
 }
--- server.c
+++ server.c
@@ -1,6 +1,73 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "common.h"
+
+#define SERVER_PORT 8080
+#define BACKLOG 5
 
 int main(void) {
-    puts("Auction server starter");
+    int server_fd;
+    int client_sock;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    char welcome[] = "Welcome to Auction System";
+    Request req;
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        close(server_fd);
+        return 1;
+    }
+
+    if (listen(server_fd, BACKLOG) < 0) {
+        perror("listen");
+        close(server_fd);
+        return 1;
+    }
+
+    printf("Server listening on port %d\n", SERVER_PORT);
+
+    while (1) {
+        client_sock = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+        if (client_sock < 0) {
+            perror("accept");
+            continue;
+        }
+
+        printf("Client connected\n");
+
+        if (send(client_sock, welcome, sizeof(welcome), 0) < 0) {
+            perror("send");
+            close(client_sock);
+            continue;
+        }
+
+        memset(&req, 0, sizeof(req));
+        if (recv(client_sock, &req, sizeof(req), 0) > 0) {
+            printf("Received request type: %d\n", req.type);
+        }
+
+        close(client_sock);
+    }
+
+    close(server_fd);
     return 0;
 }
